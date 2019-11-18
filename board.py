4735c5d33c5e423e1230a7f9220af61370bd1d9c
khrissianjoi <khrissianjoi@gmail.com>
2019-11-18 15:28:01 +0000

    boxes

--- board.py
+++ board.py
@@ -7,9 +7,10 @@ RED = 255, 0, 0, 0.8
 YELLOW = 236, 183, 24, 1
 BLUE = 24, 116, 236, 1
 GREEN = 102, 236, 24, 1
+BLUE_BACKGROUND = 209,230,238
 class Board:
     def __init__(self):
-        self.size = 30
+        self.size = 60
         self.x = 1100
         self.y = 1100
         self.gameDisplay = pygame.display.set_mode((self.x,self.y))
@@ -17,19 +18,22 @@ class Board:
     def generate_tiles(self):
         pygame.display.set_caption("Board")
 
-        self.gameDisplay.fill(YELLOW)
+        self.gameDisplay.fill(BLUE_BACKGROUND)
 
         boardLength = 15
         for i in range(1,boardLength+1):
             if i in range(7,10):
                 for z in range(1,boardLength+1):
                     pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
+                    pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
             else:
                 for z in range(1,boardLength+1):
                     if (z in range(0,7) or z in range(10,16)):
                         pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size])
+                        pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size],1)
                     else:
                         pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
+                        pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
         pygame.draw.rect(self.gameDisplay,BLACK,[self.size,self.size,boardLength*self.size,boardLength*self.size],1)
 
         pygame.display.update()
