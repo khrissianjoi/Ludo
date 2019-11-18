10bfff4bfa6915020dbe381028a7300e68a273f2
khrissianjoi <khrissianjoi@gmail.com>
2019-11-18 15:43:46 +0000

    colours

--- board.py
+++ board.py
@@ -1,13 +1,18 @@
 import pygame
 
 
-WHITE = 200,200,200
+WHITE = 255,255,255
 BLACK = 0,0,0
 RED = 255, 0, 0, 0.8
 YELLOW = 236, 183, 24, 1
-BLUE = 24, 116, 236, 1
 GREEN = 102, 236, 24, 1
 BLUE_BACKGROUND = 209,230,238
+
+TEAM_BLUE = 65, 179, 226
+TEAM_YELLOW = 251,217,132
+TEAM_RED = 241, 120, 107
+TEAM_GREEN = 90,200,174
+
 class Board:
     def __init__(self):
         self.size = 60
@@ -24,16 +29,24 @@ class Board:
         for i in range(1,boardLength+1):
             if i in range(7,10):
                 for z in range(1,boardLength+1):
+                    # horizontal white spaces
                     pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
                     pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
             else:
                 for z in range(1,boardLength+1):
                     if (z in range(0,7) or z in range(10,16)):
-                        pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size])
-                        pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size],1)
+                        # home base
+                        if i in range(0,7) and z in range(0,7):
+                            pygame.draw.rect(self.gameDisplay, TEAM_RED,[self.size*z,self.size*i,self.size,self.size])
+                        elif i in range(0,7) and z in range(10,16):
+                            pygame.draw.rect(self.gameDisplay, TEAM_BLUE,[self.size*z,self.size*i,self.size,self.size])
+                        elif i in range(10,16) and z in range(0,7):
+                            pygame.draw.rect(self.gameDisplay, TEAM_YELLOW,[self.size*z,self.size*i,self.size,self.size])
+                        else:
+                            pygame.draw.rect(self.gameDisplay, TEAM_GREEN,[self.size*z,self.size*i,self.size,self.size])
                     else:
                         pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
                         pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
-        pygame.draw.rect(self.gameDisplay,BLACK,[self.size,self.size,boardLength*self.size,boardLength*self.size],1)
+        pygame.draw.rect(self.gameDisplay,BLACK,[self.size,self.size,boardLength*self.size,boardLength*self.size],3)
 
         pygame.display.update()
