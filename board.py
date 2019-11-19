ebd8600f667ac4aeb0b1cd68abc4464f3652eb5f
khrissianjoi <khrissianjoi@gmail.com>
2019-11-19 12:29:22 +0000

    tileType

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 import pygame
-# import tile
+import tile
 
 WHITE = 255,255,255
 BLACK = 0,0,0
@@ -8,55 +8,59 @@ YELLOW = 236, 183, 24, 1
 GREEN = 102, 236, 24, 1
 
 # interface colours
-BLUE_BACKGROUND = 209,230,238
+BLUE_BACKGROUND = 209, 230, 238
 
 # team colours
 TEAM_BLUE = 65, 179, 226
-TEAM_YELLOW = 251,217,132
+TEAM_YELLOW = 251, 217, 132
 TEAM_RED = 241, 120, 107
-TEAM_GREEN = 90,200,174
+TEAM_GREEN = 90, 200, 174
 
 class Board:
     def __init__(self):
-        self.tiles = []
+        self.tiles = {}
         self.size = 60
-        self.x = 1100
-        self.y = 1100
-        self.gameDisplay = pygame.display.set_mode((self.x,self.y))
+        self.display_size_x = 1500
+        self.display_size_y = 1000
+        self.gameDisplay = pygame.display.set_mode((self.display_size_x, self.display_size_y))
 
     def generate_tiles(self):
         pygame.display.set_caption("Board")
 
         self.gameDisplay.fill(BLUE_BACKGROUND)
         boardLength = 15 # 15*15 board
-        for i in range(1,boardLength+1):
-            if i in range(7,10):
-                for z in range(1,boardLength+1):
+        for yCoord in range(1, boardLength+1):
+            if yCoord in range(7,10):
+                for xCoord in range(1, boardLength+1):
                     # path - horizontal white spaces
-                    pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
-                    pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
+                    tileType = "path"
+                    pygame.draw.rect(self.gameDisplay, WHITE,[self.size*xCoord, self.size*yCoord, self.size, self.size])
+                    pygame.draw.rect(self.gameDisplay, BLACK,[self.size*xCoord, self.size*yCoord, self.size, self.size], 1)
+                    currentTile = tile.Tile(range(self.size*xCoord,(self.size*xCoord)+61),range(self.size*yCoord,(self.size*yCoord)+61),tileType)
+                    self.tiles[currentTile] = (range(self.size*xCoord,(self.size*xCoord)+61),range(self.size*yCoord,(self.size*yCoord)+61))
             else:
-                for z in range(1,boardLength+1):
-                    if (z in range(0,7) or z in range(10,16)):
+                for xCoord in range(1,boardLength+1):
+                    # rect (left(x),top(y))
+                    if (xCoord in range(0, 7) or xCoord in range(10, 16)):
                         # home base
                         tileType = "base"
-                        if i in range(0,7) and z in range(0,7):
-                            pygame.draw.rect(self.gameDisplay, TEAM_RED,[self.size*z,self.size*i,self.size,self.size])
-                        elif i in range(0,7) and z in range(10,16):
-                            pygame.draw.rect(self.gameDisplay, TEAM_BLUE,[self.size*z,self.size*i,self.size,self.size])
-                        elif i in range(10,16) and z in range(0,7):
-                            pygame.draw.rect(self.gameDisplay, TEAM_YELLOW,[self.size*z,self.size*i,self.size,self.size])
+                        if yCoord in range(0, 7) and xCoord in range(0, 7):
+                            pygame.draw.rect(self.gameDisplay, TEAM_RED,[self.size*xCoord, self.size*yCoord, self.size, self.size])
+                        elif yCoord in range(0, 7) and xCoord in range(10, 16):
+                            pygame.draw.rect(self.gameDisplay, TEAM_BLUE,[self.size*xCoord, self.size*yCoord, self.size, self.size])
+                        elif yCoord in range(10, 16) and xCoord in range(0, 7):
+                            pygame.draw.rect(self.gameDisplay, TEAM_YELLOW,[self.size*xCoord, self.size*yCoord, self.size, self.size])
                         else:
-                            pygame.draw.rect(self.gameDisplay, TEAM_GREEN,[self.size*z,self.size*i,self.size,self.size])
+                            pygame.draw.rect(self.gameDisplay, TEAM_GREEN,[self.size*xCoord, self.size*yCoord, self.size, self.size])
                     else:
                         # path - vertical white spaces
                         tileType = "path"
-                        pygame.draw.rect(self.gameDisplay, WHITE,[self.size*z,self.size*i,self.size,self.size])
-                        pygame.draw.rect(self.gameDisplay, BLACK,[self.size*z,self.size*i,self.size,self.size],1)
-                    
+                        pygame.draw.rect(self.gameDisplay, WHITE,[self.size*xCoord, self.size*yCoord, self.size, self.size])
+                        pygame.draw.rect(self.gameDisplay, BLACK,[self.size*xCoord, self.size*yCoord, self.size, self.size], 1)
                     # append tiles to list
-                    # self.tiles.append(tile.Tile(self.x,self.y,tileType))
+                    currentTile = tile.Tile(range(self.size*xCoord,(self.size*xCoord)+61),range((self.size*yCoord),(self.size*yCoord)+61),tileType)
+                    self.tiles[currentTile] = (range(self.size*xCoord,(self.size*xCoord)+61),range((self.size*yCoord),(self.size*yCoord)+61))
 
-        pygame.draw.rect(self.gameDisplay,BLACK,[self.size,self.size,boardLength*self.size,boardLength*self.size],3)
+        pygame.draw.rect(self.gameDisplay, BLACK, [self.size, self.size, boardLength*self.size, boardLength*self.size], 3)
 
         pygame.display.update()
--- ludo.py
+++ ludo.py
@@ -1,16 +1,34 @@
 import pygame
 import board
+import datetime
 
-pygame.init()
+class Game:
+    def __init__(self):
+        self.startTime = datetime.datetime.now()
+        self.endTime = None
 
-ludo = board.Board()
-ludo.generate_tiles()
+    def endGame(self):
+        self.endTime = datetime.datetime.now()
 
-gameExit = False
-while not gameExit:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			gameExit = True
+    def main(self):
+        pygame.init()
 
-pygame.quit()
-quit()
+        ludo = board.Board()
+        ludo.generate_tiles()
+
+        gameExit = False
+        while not gameExit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                    x,y = pygame.mouse.get_pos()
+                    for tile in ludo.tiles:
+                        if x in ludo.tiles[tile][0] and y in ludo.tiles[tile][1]:
+                            print(tile.tileType)
+                            break
+        pygame.quit()
+        quit()
+
+playing = Game()
+playing.main()
